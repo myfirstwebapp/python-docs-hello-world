1fe5186a724ede645ecdb85b5ce2bb1efb49cc78
myfirstwebapp <zakrezaye@hotmail.com>
2024-05-29 11:13:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World This is my first website and I am very happy for it!"
-    <img src="logo_securo.png" alt="SAP Logo" width="500px" height="100px"/>
